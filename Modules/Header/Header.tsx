f46d00a7e4d10a2258642d883691c160b5cf2c5f
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2022-12-17 13:58:27 -0800

    add Section, Header

---
+++ Modules/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Section } from "../../components/Section";
+import { Center } from "@chakra-ui/react";
+
+export const Header: React.FC = () => {
+  return (
+    <Section bg="gray.500">
+      <Center color="white">Johnny Nguyen Portfolio</Center>
+    </Section>
+  );
+};
